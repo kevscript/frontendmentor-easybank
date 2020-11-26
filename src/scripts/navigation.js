328ed8a5f6ce9c5db2f0910fdbd57a54decb6847
Kevin Ostafinski <kevinostafinski@gmail.com>
2020-11-26 23:26:32 +0100

    handle mobile nav state on window resize

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -3,20 +3,32 @@ const burgerClosed = document.querySelector('.burger-closed');
 const burgerOpened = document.querySelector('.burger-opened');
 const mobileNav = document.querySelector('.navigation-mobile');
 
+const initialNavState = () => {
+  burgerOpened.style.display = "none";
+  mobileNav.style.display = "none";
+  burgerClosed.style.display = "block";
+  burger.classList.remove('is-opened')
+  burger.classList.add('is-closeed');
+}
+
+// 
 burger.addEventListener('click', () => {
   const isOpen = burger.classList.contains('is-opened')
-  console.log('is open:' + isOpen)
   if (isOpen) {
-    burgerOpened.style.display = "none";
-    mobileNav.style.display = "none";
-    burgerClosed.style.display = "block";
-    burger.classList.remove('is-opened')
-    burger.classList.add('is-closeed');
+    initialNavState()
   } else {
     burgerClosed.style.display = "none";
     burgerOpened.style.display = "block";
     mobileNav.style.display = "block";
     burger.classList.remove('is-closed')
     burger.classList.add('is-opened');
+  }
+})
+
+// set mobile navigation to its initial closed state 
+// when window gets wider than 1170px breakpoint 
+window.addEventListener('resize', () => {
+  if (window.innerWidth >= 1170) {
+    initialNavState()
   }
 })
